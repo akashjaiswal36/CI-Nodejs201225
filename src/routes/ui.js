7798bdb148e88cbd78a5a2bc7c254489f972cf21
akashjaiswal36 <akash.jaiswal36@gmail.com>
2025-12-25 00:11:50 +0530

    owrking on docker desktop

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
       </head>
       <body>
         <div class="card">
-          <h2>🚀 DevOps Platform Demo, Hi Im EKS</h2>
+          <h2>🚀 DevOps Platform Demo, Chrismas eve</h2>
           <p><b>Status:</b> UP</p>
           <p><b>Version:</b> ${process.env.APP_VERSION || 'v1'}</p>
           <p><b>Environment:</b> ${process.env.ENV || 'dev'}</p>
